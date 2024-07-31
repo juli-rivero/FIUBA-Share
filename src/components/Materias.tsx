8134fd839ba51f459f283b29229fe6a1a7e04392
Juli <jarivero@fi.uba.ar>
2024-07-31 18:46:18 -0300

    Estructura materias

--- src/components/Materias.tsx
+++ src/components/Materias.tsx
@@ -1,11 +1,30 @@
-import Header from "./Header"
+import materias from "../data/materias.json";
+
+import Header from "./Header";
+import { Card, CardContent, Stack } from "@mui/joy";
+import { Unstable_Grid } from "@mui/system";
 
 function Materias() {
-    return (
-        <>
-        <Header />
-        </>
-    )
+  return (
+    <Stack sx={{ height: "100%" }} gridTemplateRows="auto 2fr">
+      <Header />
+      <Unstable_Grid
+        sx={{ height: "100%" }}
+        container
+        gap={2}
+        alignItems="center"
+        justifyContent="center"
+      >
+        {materias.map(({ nombre }) => (
+          <Unstable_Grid>
+            <Card variant="soft" size="lg">
+              <CardContent>{nombre}</CardContent>
+            </Card>
+          </Unstable_Grid>
+        ))}
+      </Unstable_Grid>
+    </Stack>
+  );
 }
 
-export default Materias
+export default Materias;
