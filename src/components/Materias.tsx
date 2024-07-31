ded3d2537211f6c68ce5e60dbf37facef50d3211
Juli <jarivero@fi.uba.ar>
2024-07-31 18:58:59 -0300

    optimizacion materias

--- src/components/Materias.tsx
+++ src/components/Materias.tsx
@@ -1,7 +1,7 @@
 import materias from "../data/materias.json";
 
 import Header from "./Header";
-import { Card, CardContent, Stack } from "@mui/joy";
+import { Stack, Link } from "@mui/joy";
 import { Unstable_Grid } from "@mui/system";
 
 function Materias() {
@@ -11,16 +11,14 @@ function Materias() {
       <Unstable_Grid
         sx={{ height: "100%" }}
         container
-        gap={2}
+        gap={4}
         alignItems="center"
         justifyContent="center"
       >
-        {materias.map(({ nombre }) => (
-          <Unstable_Grid>
-            <Card variant="soft" size="lg">
-              <CardContent>{nombre}</CardContent>
-            </Card>
-          </Unstable_Grid>
+        {materias.map(({ nombre, id }) => (
+            <Link underline="none" color="neutral" variant="soft" padding="1.5rem" borderRadius={4} href={`./fechas?materia=${id}`}>
+            {nombre}
+            </Link>
         ))}
       </Unstable_Grid>
     </Stack>
