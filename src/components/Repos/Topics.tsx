01039aeba28058a04177a9efd01eab1fa0bdcc66
Juli <jarivero@fi.uba.ar>
2024-08-23 19:32:26 -0300

    clipboard hecho

--- src/components/Repos/Topics.tsx
+++ src/components/Repos/Topics.tsx
@@ -1,21 +1,61 @@
-import { RiPencilLine } from "react-icons/ri";
-import { Chip, Stack, Typography } from "@mui/joy";
+import { RiStickyNoteAddLine, RiCheckDoubleLine } from "react-icons/ri";
+import { Chip, ChipDelete, Stack, Tooltip, Typography } from "@mui/joy";
+import { useState } from "react";
 
 const Topic = ({ value }: { value: string }) => {
+  const [openTooltip, setOpenTooltip] = useState<string | null>(null);
+  const [hover, setHover] = useState(false);
   return (
+    <Tooltip
+      placement="top"
+      arrow
+      open={openTooltip != null}
+      color={openTooltip === "¡Copiado!" ? "primary" : "warning"}
+      onOpen={() => setTimeout(() => setOpenTooltip(null), 3000)}
+      title="¡Copiado!"
+    >
       <Chip
-        component="span"
         variant="outlined"
-        sx={{ borderRadius: 2, bgcolor: "inherit", fontFamily:"monospace" }}
+        sx={{
+          borderRadius: 2,
+          bgcolor: "inherit",
+          fontFamily: "monospace",
+          gap: ".5rem",
+        }}
+        endDecorator={
+          <ChipDelete style={{ background: "inherit", cursor: "default" }}>
+            {openTooltip ? (
+              <RiCheckDoubleLine />
+            ) : (
+              <RiStickyNoteAddLine
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+                onClick={() => {
+                  navigator.clipboard
+                    .writeText(value)
+                    .then(() => setOpenTooltip("¡Copiado!"))
+                    .catch(() => setOpenTooltip("No se pudo copiar"));
+                  setHover(false)
+                }}
+                style={{
+                  padding: ".1rem",
+                  cursor: "pointer",
+                  borderRadius: "1px",
+                  background: hover ? "#aaaa" : "inherit",
+                  boxSizing: "border-box",
+                }}
+              />
+            )}
+          </ChipDelete>
+        }
       >
         {value}
-        <RiPencilLine onClick={()=>{
-
-        }} style={{ marginLeft: ".5rem" }} />
       </Chip>
-)}
+    </Tooltip>
+  );
+};
 
-function Topics({topics}: {topics:string[]}) {
+function Topics({ topics }: { topics: string[] }) {
   return (
     <Stack
       direction="row"
@@ -25,7 +65,9 @@ function Topics({topics}: {topics:string[]}) {
       gap={0.5}
     >
       <Typography marginRight={1}>Topics:</Typography>
-      {topics.map(value=><Topic key={value} value={value.toLowerCase()} />)}
+      {topics.map((value) => (
+        <Topic key={value} value={value.toLowerCase()} />
+      ))}
     </Stack>
   );
 }
